e7f4f8012be2a6975e3d917081d0786dde821e9c
asanoyu <s15t201@stu.kagawa-u.ac.jp>
2016-10-14 20:45:52 +0900

    Button test

--- app/src/main/java/com/example/gmvproject/GMVActive.java
+++ app/src/main/java/com/example/gmvproject/GMVActive.java
@@ -2,13 +2,27 @@ package com.example.gmvproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GMVActive extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gmvactive);
+
+        setContentView(R.layout.test);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setText("ABC");
+            }
+        });
+
     }
     // test
 }
