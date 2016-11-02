fbfe31e6a9fddd5d795a96ceb1a7bc21036939a4
morivden <fuziyamavolcano@yahoo.co.jp>
2016-11-02 21:31:06 +0900

    add GameView.java

---
+++ app/src/main/java/com/example/gmvproject/GameView.java
@@ -0,0 +1,115 @@
+package com.example.gmvproject;
+
+/**
+ * Created by マグネシウム on 2016/11/02.
+ */
+/*
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private static final long DRAW_INTERVAL = 1000 / 100;
+
+    private class DrawThread extends Thread {
+        private final AtomicBoolean isFinished = new AtomicBoolean(false);
+
+        public void finish() {
+            isFinished.set(true);
+        }
+
+        @Override
+        public void run() {
+            SurfaceHolder holder = getHolder();
+
+            while (!isFinished.get()) {
+                if (holder.isCreating()) {
+                    continue;
+                }
+                Canvas canvas = holder.lockCanvas();
+                if (canvas == null) {
+                    continue;
+                }
+
+                drawGame(canvas);
+
+                holder.unlockCanvasAndPost(canvas);
+
+                synchronized (this) {
+                    try {
+                        wait(DRAW_INTERVAL);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        }
+    }
+
+    private DrawThread drawThread;
+
+    public void startDrawThread() {
+        stopDrawThread();
+
+        drawThread = new DrawThread();
+        drawThread.start();
+    }
+
+    public boolean stopDrawThread() {
+        if (drawThread == null) {
+            return false;
+        }
+        drawThread.finish();
+        drawThread = null;
+
+        return true;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(
+            SurfaceHolder holder, int format, int width, int height) {
+        startDrawThread();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        stopDrawThread();
+    }
+
+    public GameView(Context context) {
+        super(context);
+
+        droidBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.droid);
+        droid = new Droid(droidBitmap, 0, 0, droidCallback);
+
+        getHolder().addCallback(this);
+    }
+
+
+    private void drawGame(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        if (ground == null) {
+            int top = height - GROUND_HEIGHT;
+            ground = new Ground(0, top, width, height);
+        }
+        droid.move();
+        ground.move(GROUND_MOVE_TO_LEFT);
+        droid.draw(canvas);
+        ground.draw(canvas);
+    }
+}
+*/
