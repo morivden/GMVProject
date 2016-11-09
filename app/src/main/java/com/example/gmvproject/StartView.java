20aee9cb57f75d53c32de2d48d83120efefe3812
morivden <fuziyamavolcano@yahoo.co.jp>
2016-11-09 20:57:50 +0900

    change StartView.java

--- app/src/main/java/com/example/gmvproject/StartView.java
+++ app/src/main/java/com/example/gmvproject/StartView.java
@@ -5,29 +5,34 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.view.View;
+import android.widget.ImageView;
+import android.content.Context;
 
 /**
  * Created by 太田圭祐 on 2016/11/04.
  */
-public class StartView {
-    private final Paint paint = new Paint();
+public class StartView extends View {
+    //private final Paint paint = new Paint();
 
-    public final Bitmap bitmap;
-    public final Rect rect;
+    private ImageView screenImage;
 
-    public StartView( Bitmap bitmap, int width, int height )
-    {
-        this.bitmap = bitmap;
+    //public final Bitmap bitmap;
+    //public final Rect rect;
 
+    public StartView(Context context) {
+        super(context);
+        /*
         int left = (width - bitmap.getWidth()) / 2;
         int top = (height - bitmap.getHeight()) / 2;
         int right = left + bitmap.getWidth();
         int bottom = top + bitmap.getHeight();
         rect = new Rect(left, top, right, bottom);
+        */
     }
 
-    public void draw(Canvas canvas)
-    {
-        canvas.drawBitmap(bitmap, rect.left, rect.top, paint);
+    @Override
+    public void onDraw(Canvas canvas) {
+        screenImage.draw(canvas);
     }
 }
