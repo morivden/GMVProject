215fa46602ed86798851797769f9d1774ccb4669
asanoyu <s15t201@stu.kagawa-u.ac.jp>
2016-11-10 10:10:51 +0900

    Enable to draw titleView

--- app/src/main/java/com/example/gmvproject/TitleView.java
+++ app/src/main/java/com/example/gmvproject/TitleView.java
@@ -1,53 +1,38 @@
 package com.example.gmvproject;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.os.Bundle;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-
-import java.util.zip.Inflater;
 
 /**
  * Created by ito_takumi on 2016/11/04.
  */
 
-public class TitleView extends View {
+public class TitleView extends FrameLayout {
 
     private Button buttonPush;
     private ImageView backgroundImage;
     private LayoutInflater inflater;
-    View view;
+    private View rootView;
 
     public TitleView(Context context) {
         super(context);
 
         this.inflater = LayoutInflater.from(context);
 
-        view = inflater.inflate(R.layout.title, null);
+        this.rootView = inflater.inflate(R.layout.title, this);
 
-        backgroundImage = (ImageView) view.findViewById(R.id.imageView);
+        this.backgroundImage = (ImageView) findViewById(R.id.imageView);
 
-        buttonPush = (Button) view.findViewById(R.id.btn_push);
-        buttonPush.setOnClickListener(new View.OnClickListener() {
+        this.buttonPush = (Button) findViewById(R.id.btn_push);
+        this.buttonPush.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 buttonPush.setText("スタート画面に移動したよ!!");
             }
         });
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        invalidate();
     }
 }
