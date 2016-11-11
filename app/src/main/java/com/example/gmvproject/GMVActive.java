30a1c618df24de21ad80ccdb45ed86181af127eb
Matrix5411 <gazigazi.main5@gmail.com>
2016-11-11 20:47:22 +0900

    add StartView.java and Active is NullPointerException

--- app/src/main/java/com/example/gmvproject/GMVActive.java
+++ app/src/main/java/com/example/gmvproject/GMVActive.java
@@ -2,13 +2,16 @@ package com.example.gmvproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.app.Activity;
 import android.view.Window;
+import android.widget.FrameLayout;
+
 
 public class GMVActive extends Activity {
     Button button;
+
+    FrameLayout viewList = new FrameLayout(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +20,10 @@ public class GMVActive extends Activity {
         setContentView(R.layout.activity_gmvactive);
 
         TitleView tv = new TitleView(this);
-        setContentView(tv);
+        viewList.addView(tv);
+        setContentView(viewList);
+
+        //StartView sv = new StartView(this);
     }
     // test
     // console git test
